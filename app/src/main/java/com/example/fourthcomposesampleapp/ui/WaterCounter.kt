431c34ac53724a6e47103e2f40aa6eaf6bd14303
HamidReza Farahani <h.farahani>
2023-08-24 17:42:04 +0430

    enable recomposition effect

--- app/src/main/java/com/example/fourthcomposesampleapp/ui/WaterCounter.kt
+++ app/src/main/java/com/example/fourthcomposesampleapp/ui/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -13,7 +17,9 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(
     modifier: Modifier = Modifier
 ) {
-    var count = 0
+    var count: Int by rememberSaveable {
+        mutableStateOf(0)
+    }
     Column(
         modifier = modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
